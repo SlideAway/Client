f89a0d9277d81cfb90ae09f1f9a4398480b3347e
YoonJeongMin <gkszm636@gmail.com>
2019-02-08 21:41:03 +0900

    Demo

--- Main.java
+++ Main.java
@@ -67,44 +67,58 @@ public class Main {
         try {
             Socket socket = new Socket("localhost", 9999);
             Scanner scan = new Scanner(System.in);
-            //BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             InputStream is = socket.getInputStream();
             final ObjectInputStream ois = new ObjectInputStream(is);
             OutputStream os = socket.getOutputStream();
             final ObjectOutputStream oos = new ObjectOutputStream(os);
             System.out.println("서버에 연결중입니다.....");
-            //BufferedWriter Fout = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            //BufferedReader Fin = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PublicKey pubKey = null;
-            PrivateKey priKey = null;
+            String outputmessage;
+            byte[] buffer;
 
-            //서버 공개키 수신
-            byte[] ServerKeyByte = (byte[])ois.readObject();
+            //공개키쌍 생성
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024);
+            KeyPair keypair = kpg.genKeyPair();
+            final PublicKey pubKey = keypair.getPublic();
+            final PrivateKey priKey = keypair.getPrivate();
+
+            //여기서부터 평문전송
+            //서버 공개키 수신 및 복구
+            buffer = (byte[])ois.readObject();
             KeyFactory kf = KeyFactory.getInstance("RSA");
-            PublicKey serverkey = kf.generatePublic(new X509EncodedKeySpec(ServerKeyByte));
+            PublicKey serverkey = kf.generatePublic(new X509EncodedKeySpec(buffer));
+
             //클라이언트 공개키 전송
-            byte[] pubkeybyte = pubKey.getEncoded();
-            oos.writeObject(pubkeybyte);
+            oos.writeObject(pubKey.getEncoded());
             oos.flush();
+
+            //여기서부터 공개키로 암호화 전송
             //비밀키 수신
-            byte[] encryptedsecretkeybyte = (byte[])ois.readObject();
-            byte[] secretkeybyte = RSADecrypt(priKey, encryptedsecretkeybyte);
-            SecretKey secretkey = new SecretKeySpec(secretkeybyte, "AES");
+            buffer = (byte[])ois.readObject();
+            byte[] Skeybyte = RSADecrypt(priKey, buffer);
+            SecretKey Skey = new SecretKeySpec(Skeybyte, "AES");
+
             //검증을 위한 비밀키 전송
-            oos.writeObject(RSAEncrypt(serverkey, secretkey));
+            oos.writeObject(RSAEncrypt(serverkey, Skey));
             oos.flush();
             boolean veri = (boolean)ois.readObject();
 
+            if(veri == true) {
+                System.out.println("키 교환 성공");
+            }
+            else {
+                System.out.println("키 교환 실패");
+                System.exit(0);
+            }
+
             for (; ; ) {
-                byte[] encryptmessage = (byte[])ois.readObject();
-                byte[] decryptmessage = AESdecrypt(secretkey, encryptmessage);
-                String inputmessage = new String(decryptmessage);
+                buffer = (byte[])ois.readObject();
+                String inputmessage = new String(AESdecrypt(Skey, buffer));
                 if(isStringDouble(inputmessage)) {
                     if(Integer.parseInt(inputmessage) == 1) {//1일때 입력값을 받고 서버에 전송
                         System.out.print("입력 : ");
-                        String outputmessage = scan.nextLine();
-                        oos.writeObject(AESencrypt(secretkey, outputmessage.getBytes()));
+                        outputmessage = scan.nextLine();
+                        oos.writeObject(AESencrypt(Skey, outputmessage.getBytes()));
                         oos.flush();
                     } else if(Integer.parseInt(inputmessage) == 2) {
                         System.out.println("프로그램을 종료합니다. ");
